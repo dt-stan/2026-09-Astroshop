610b653fd089c645a9b34f5cb3185bc13d49c600
Stan Switalski <stanley.switalski@dynatrace.com>
2026-09-10 05:09:41 +0000

    Modifying the Astroshop and Codespace config to launch at start automatically

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,4 +5,12 @@ echo "Verifying Docker is available..."
 docker version
 docker compose version
 
-echo "Codespace ready. Docker and Docker Compose are up and running."
+echo "Codespace ready. Docker and Docker Compose are up and running. Will start the Astroshop web store"
+
+gh codespace ports visibility 8080:public -c $CODESPACE_NAME
+
+echo 'Please run the following command to bring the Astroshop Web Stone online:'
+echo
+echo '  docker compose --env-file .env --env-file .env.override \'
+echo '    -f compose.yaml -f compose.observability.yaml -f compose.extras.yaml \'
+echo '    up --force-recreate --remove-orphans --detach'
